7bb28548782ea6edf65c4617f1889c2727912059
Alexandra Dumitrachi <dumitrachi.alexandra@yahoo.com>
2020-04-11 19:58:04 +0300

    homework 1 s8 refacuta

--- Saptamana 8/P1/Practice/s81.js
+++ Saptamana 8/P1/Practice/s81.js
@@ -1,39 +1,33 @@
-function Calculate(a, b) {
+function calculate(a, b) {
   if (a === b) {
     return a * b;
   }
-  {
-    return a + b;
-  }
+  return a + b;
 }
 
-console.log(Calculate(5, 10));
-console.log(Calculate(10, 10));
+console.log(calculate(5, 10));
+console.log(calculate(10, 10));
 
 //2
 
-function CheckThis(prm1, prm2) {
+function checkThis(prm1, prm2) {
   if ((prm1 && prm2 === 30) || prm1 + prm2 === 30) {
     return true;
   }
-  {
-    return false;
-  }
+  return false;
 }
 
-console.log(CheckThis(30, 30));
-console.log(CheckThis(15, 15));
-console.log(CheckThis(10, 15));
+console.log(checkThis(30, 30));
+console.log(checkThis(15, 15));
+console.log(checkThis(10, 15));
 
 //3
 
-let checkString = param1 => {
+let checkString = (param1) => {
   if (param1.includes("JS")) {
     return param1;
   }
-  {
-    return `JS ${param1}`;
-  }
+  return `JS ${param1}`;
 };
 
 console.log(checkString("smth"));
@@ -41,7 +35,7 @@ console.log(checkString("JS me"));
 
 //4
 
-let RemoveDouble = theparam => {
+let removeDouble = (theparam) => {
   let host = "";
   for (var i = 0; i < theparam.length; i++) {
     if (host.indexOf(theparam[i]) == -1) {
@@ -50,12 +44,12 @@ let RemoveDouble = theparam => {
   }
   return host;
 };
-console.log(RemoveDouble("xxccffeyhhh"));
-console.log(RemoveDouble("00223344"));
+console.log(removeDouble("xxccffeyhhh"));
+console.log(removeDouble("00223344"));
 
 //5
 
-let findWord = str => {
+let findWord = (str) => {
   let strSplit = str.split(" ");
   let result = 0;
   for (let i = 0; i < strSplit.length; i++) {
@@ -69,89 +63,72 @@ console.log(findWord("This is the longest word"));
 
 //6
 
-let Stars = rows => {
-  for (i = 1; i <= rows; i++) {
-    let str = "";
-    for (let index = 1; index <= i; index++) {
-      str += "*";
-    }
-    console.log(str);
+function stars(rows) {
+  let result = [];
+  for (let i = 0; i <= rows; i++) {
+    let star = "*".repeat(i);
+    result.push(star);
   }
-};
-
-Stars(5);
+  return result.join("\n");
+}
+console.log(stars(5));
 
 //7
 
-const negativeNumbers = [];
-
 function extractNegativeNumbers(numbers) {
+  const negativeNumbers = [];
   for (i = 0; i < numbers.length; i++) {
     if (numbers[i] < 0) {
       negativeNumbers.push(numbers[i]);
     }
   }
-  console.log(negativeNumbers);
+  return negativeNumbers;
 }
-extractNegativeNumbers([1, 2, -5, 4, -6]);
+console.log(extractNegativeNumbers([1, 2, -5, 4, -6]));
 
 //8
 
-let calculate = (param1, param2, param3) => {
+let calculateMath = (param1, param2, param3) => {
   if (param3 === "add") {
-    console.log(param1 + param2);
+    return param1 + param2;
   } else if (param3 === "substract") {
-    console.log(param1 - param2);
+    return param1 - param2;
   } else if (param3 === "multiply") {
-    console.log(param1 * param2);
+    return param1 * param2;
   } else if (param3 === "divide") {
-    console.log(param1 / param2);
+    return param1 / param2;
+  } else {
+    return "This arithmetic operation cannot be done.";
   }
 };
 
-calculate(2, 5, "add");
-calculate(10, 8, "substract");
-calculate(10, 2, "divide");
-calculate(2, 3, "multiply");
+console.log(calculateMath(2, 5, "add"));
+console.log(calculateMath(10, 8, "substract"));
+console.log(calculateMath(10, 2, "divide"));
+console.log(calculateMath(2, 3, "multiply"));
 
 //9
 
-let isDiv = num => {
+let isDiv = (num) => {
   if (num % 3 === 0 && num % 5 === 0) {
-    console.log("BOTH");
+    return "BOTH";
   } else if (num % 5 === 0) {
-    console.log("FIVE");
+    return "FIVE";
   } else if (num % 3 === 0) {
-    console.log("THREE");
+    return "THREE";
   } else {
-    console.log(num);
+    return num;
   }
 };
 
-isDiv(15);
-isDiv(9);
-isDiv(7);
+console.log(isDiv(15));
+console.log(isDiv(9));
+console.log(isDiv(7));
 
 //9 Master
 
-let ShowDate = param1 => {
+let showDate = (param1) => {
   let today = new Date();
-  let toDay = "Azi este : " + today.getDay();
-  if (today.getDay() === 0) {
-    console.log("Astazi este : Duminica");
-  } else if (today.getDay() === 1) {
-    console.log("Astazi este : Luni");
-  } else if (today.getDay() === 2) {
-    console.log("Astazi este : Marti");
-  } else if (today.getDay() === 3) {
-    console.log("Astazi este : Miercuri");
-  } else if (today.getDay() === 4) {
-    console.log("Astazi este : Joi");
-  } else if (today.getDay() === 5) {
-    console.log("Astazi este : Vineri");
-  } else if (today.getDay() === 6) {
-    console.log("Astazi este : Sambata");
-  }
   let Hour =
     "Ora este : " +
     today.getHours() +
@@ -159,54 +136,73 @@ let ShowDate = param1 => {
     today.getMinutes() +
     " : " +
     today.getSeconds();
-
-  console.log(Hour);
-};
-
-ShowDate("Apr 03, 2020");
-
-//10
-
-let validPin = pin => {
-  console.log(/^([0-9]{4}|[0-9]{6})$/.test(pin));
-};
-
-validPin("1234");
-validPin("12345");
-validPin("z23f");
-
-// for(i=0; i <= pin.length; i++){
-//     if(pin.length === 4){
-//         console.log(true);
-//     }else if(pin[i] !== [0-9] || pin.length < 4){
-//         console.log(false);
-//     }
-// }
-
-//eu facusem asa dar la litere nu stiam ce sa pun ca sa verifice...
-
-//11
-
-let removeVowels = word => {
-  consonants = word.match(/[^aeiou]/gi);
-  console.log(consonants.join(""));
-};
-
-removeVowels("Hey I am developer");
-
-//12
-
-let isSquareNumber = num => {
-  if (num > 0 && Math.sqrt(num) % 1 === 0) {
-    console.log(true);
-  } else {
-    console.log(false);
+  let toDay = "Azi este : " + today.getDay();
+  if (today.getDay() === 0) {
+    return "Astazi este : Duminica " + "\n" + Hour;
+  } else if (today.getDay() === 1) {
+    return "Astazi este : Luni " + "\n" + Hour;
+  } else if (today.getDay() === 2) {
+    return "Astazi este : Marti " + "\n" + Hour;
+  } else if (today.getDay() === 3) {
+    return "Astazi este : Miercuri " + "\n" + Hour;
+  } else if (today.getDay() === 4) {
+    return "Astazi este : Joi " + "\n" + Hour;
+  } else if (today.getDay() === 5) {
+    return "Astazi este : Vineri " + "\n" + Hour;
+  } else if (today.getDay() === 6) {
+    return "Astazi este : Sambata " + "\n" + Hour;
   }
 };
 
-isSquareNumber(-1);
-isSquareNumber(25);
-isSquareNumber(3);
+console.log(showDate("Apr 03, 2020"));
+
+//10
+
+let validPin = (pin) => {
+  return /^([0-9]{4}|[0-9]{6})$/.test(pin);
+};
+
+console.log(validPin("1234"));
+console.log(validPin("12345"));
+console.log(validPin("z23f"));
+
+//method 2
+
+const validatePin = (pin) => {
+  if (pin.length === 4 || pin.length === 6) {
+    if (isNaN(pin)) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+};
+
+console.log(validatePin("1234"));
+console.log(validatePin("12345"));
+console.log(validatePin("z23f"));
+
+//11
+
+let removeVowels = (word) => {
+  consonants = word.match(/[^aeiou]/gi);
+  return consonants.join("");
+};
+
+console.log(removeVowels("Hey I am developer"));
+
+//12
+
+let isSquareNumber = (num) => {
+  if (num > 0 && Math.sqrt(num) % 1 === 0) {
+    return true;
+  }
+  return false;
+};
+
+console.log(isSquareNumber(-1));
+console.log(isSquareNumber(25));
+console.log(isSquareNumber(3));
 
 //13
 
@@ -224,10 +220,9 @@ let isAnagram = (first, second) => {
       .split("")
       .sort()
       .join("")
-  ) {
-    console.log(true);
-  }
+  )
+    return true;
 };
 
-isAnagram("School master", "The class room");
-isAnagram("silent", "listen");
+console.log(isAnagram("School master", "The class room"));
+console.log(isAnagram("silent", "listen"));
