8015beecc3f21036f1977333db20228079214a71
Alexandra Dumitrachi <dumitrachi.alexandra@yahoo.com>
2020-04-11 20:16:01 +0300

    homework refacut

--- Saptamana 8/P2/Practice/s82.js
+++ Saptamana 8/P2/Practice/s82.js
@@ -6,7 +6,7 @@
 1. Implementati o functie care accepta ca parametru o valoare numerica si returneaza suma numerelor de la 1 pana la valoarea specificata
 */
 
-let Sumof = num => {
+let Sumof = (num) => {
   let sum = 0;
   for (i = 0; i <= num; i++) {
     sum += i;
@@ -20,7 +20,7 @@ Sumof(7);
 2. Implementati o functie care accepta ca parametru un string si returneaza cel mai lung cuvant din acel string.
 */
 
-let findWord = str => {
+let findWord = (str) => {
   let strSplit = str.split(" ");
   let result = 0;
   for (let i = 0; i < strSplit.length; i++) {
@@ -35,7 +35,7 @@ console.log(findWord("This is the longest word"));
 /*
 3. Implementati o functie care accepta ca parametru un string si ii face 'reverse'
 */
-let ReverseStr = str => {
+let ReverseStr = (str) => {
   let SplStr = str.split("");
   let RevStr = SplStr.reverse();
   let JoinStr = RevStr.join("");
@@ -48,8 +48,8 @@ ReverseStr("This is a reversed string.");
 4. Implementati o functie care accepta ca parametru un string si inlocuieste fiecare litera din acesta cu urmatoarea litera din alfabet
 */
 
-let ChangeLetter = letters => {
-  return letters.replace(/[a-zA-Z]/g, function(x) {
+let ChangeLetter = (letters) => {
+  return letters.replace(/[a-zA-Z]/g, function (x) {
     return String.fromCharCode(x.charCodeAt(0) + 1);
   });
 };
@@ -61,7 +61,7 @@ console.log(ChangeLetter("abcde"));
 Exemplu: input: 64  ->  output: 1:4
 */
 
-let ConvertTime = totalMinutes => {
+let ConvertTime = (totalMinutes) => {
   let n = totalMinutes;
   let divideH = n / 60;
   let hours = Math.floor(divideH);
@@ -76,11 +76,11 @@ ConvertTime(64);
 6. Implementati o functie care accepta ca parametru un string si returneaza string-ul cu toate literele ordonate alfabetic
 */
 
-let SortAplhabetical = someString =>{
-    let splitstr = someString.split("");
-    let sortstr = splitstr.sort();
-    console.log(sortstr.join(""));
-}
+let SortAplhabetical = (someString) => {
+  let splitstr = someString.split("");
+  let sortstr = splitstr.sort();
+  console.log(sortstr.join(""));
+};
 
 SortAplhabetical("sort this string");
 SortAplhabetical("gfedcba");
@@ -92,34 +92,18 @@ Exemplu: input: "+a+b+c+"   ->   output: true
 Exemply: input: "+ab+c+d+"  ->   output: false
 */
 
-let CheckSign = aString =>{
-    let check = aString.charAt(i)
-    for(i=0; i< aString.length; i+=2)  
-    if(check === "+"){
-        return true;
-    }else{
-        return false;
+let checkIf = (str) => {
+  let splittedStr = str.split("");
+  for (i = 1; i < splittedStr.length - 1; i += 2) {
+    if (!str.match(/[a-z]/i)) {
+      return false;
     }
-}
-
-console.log(CheckSign("+ab+c+d"));
-console.log(CheckSign("+a+b+c"));
-
-
-// let CheckSign = aString =>{
-//     let check = aString.charAt(i)
-//     i=0;
-//     while(i< aString.length) 
-//     if(check === "+"){
-//         return true;
-//     }else{
-//         return false;
-//     }
-//     i+=2
-// }
-
-// console.log(CheckSign("+a+b+c"));
-// console.log(CheckSign("+ab+c+d"));
-
-
-//imi da niste erori ciudate la ex asta daca il pun pe jsfiddle sau codepen
+    if (splittedStr[i - 1] === "+" && splittedStr[i + 1] === "+") {
+      continue;
+    }
+    return false;
+  }
+  return true;
+};
+console.log(checkIf("+a+b+c+"));
+console.log(checkIf("+ab+c+d"));
